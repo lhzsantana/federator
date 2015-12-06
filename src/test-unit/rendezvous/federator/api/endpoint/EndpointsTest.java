71ce3d21d211a5df73fe1445f578acd8f148d948
Luiz Santana <luiz.santana@socialbase.com.br>
2015-12-06 16:08:58 -0200

    teste mockito

--- src/test-unit/rendezvous/federator/api/endpoint/EndpointsTest.java
+++ src/test-unit/rendezvous/federator/api/endpoint/EndpointsTest.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import com.fasterxml.jackson.core.JsonParseException;
+
+import rendezvous.federator.api.endpoint.impl.MappingEndpoint;
 
 public class EndpointsTest {
 
@@ -14,6 +17,15 @@ public class EndpointsTest {
 	@Test
 	public void doNothing() throws JsonParseException, IOException, Exception {
 
+		MappingEndpoint endpoint = Mockito.mock(MappingEndpoint.class);
+		
+		String json="{}";
+		
+		endpoint.extractElements(json);
+		endpoint.extractEntities(json);
+		endpoint.extractEntity(json);
+		endpoint.extractValues(json);
+		
 		logger.info("Do nothing");
 	}
 
